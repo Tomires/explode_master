c22e65608210e55e4bab85b18e11bdcf75aad81d
Tomires <me@tomires.eu>
2017-04-10 17:45:33 +0200

    Added game loop

--- explode_master.ino
+++ explode_master.ino
@@ -34,48 +34,44 @@ void loop() {
   // aktuálního času od zapnutí Arduina
   // v sekundách
 
-  long cas = millis()/1000;
+  /* SETUP MODE */
+  if(digitalRead(KEY) == LOW){
 
-  // výpočet číslic pro jednotlivé pozice
-  // na displeji pro zobrazení času
-  // např. první pozice udává desítky minut
-  // pro 1000 vteřin: 1000s/60=16min/10=1
-  // druhá pozice udává jednotky minut
-  // pro 1000 vteřin: 1000s/60=16min%10=6
-  // znaménko / je dělení, které vrací celou číslici
-  // znaménko % je zbytek po dělení
-
-  if(digitalRead(BTN) == LOW){
-    disp.showNumberDec(analogRead(POT), false);
-    delay(10);
   }
-  else {
-    vypis[0] = disp.encodeDigit((cas/60)/10);
-    vypis[1] = disp.encodeDigit((cas/60)%10);
-    vypis[2] = disp.encodeDigit((cas%60)/10);
-    vypis[3] = disp.encodeDigit((cas%60)%10);
+
+  /* GAME MODE */
+  else{
+    delay(1000);
+    time = millis()/1000;
+    // výpočet číslic pro jednotlivé pozice
+    // na displeji pro zobrazení času
+    // např. první pozice udává desítky minut
+    // pro 1000 vteřin: 1000s/60=16min/10=1
+    // druhá pozice udává jednotky minut
+    // pro 1000 vteřin: 1000s/60=16min%10=6
+    // znaménko / je dělení, které vrací celou číslici
+    // znaménko % je zbytek po dělení
 
     // výpis informací na displej
-    disp.setSegments(vypis);
-
+    disp.setSegments(display_output);
     if(digitalRead(KEY) == LOW){
       analogWrite(5, 30);
     }
     else {
       analogWrite(5, 0);
     }
-
-    analogWrite(6, 0);
-    delay(500);
-
     // pro zobrazení dvojtečky mezi číslicemi
     // je nutné k pozici 1 přičíst hodnotu 128
-    vypis[1] = vypis[1]+128;
-
-    disp.setSegments(vypis);
 
     analogWrite(6, 30);
-    delay(500);
-
+    
   }
+
+  display_output[0] = disp.encodeDigit((time/60)/10);
+  display_output[1] = disp.encodeDigit((time/60)%10);
+  display_output[2] = disp.encodeDigit((time%60)/10);
+  display_output[3] = disp.encodeDigit((time%60)%10);
+  display_output[1] = display_output[1]+128;
+  disp.setSegments(display_output);
+
 }
